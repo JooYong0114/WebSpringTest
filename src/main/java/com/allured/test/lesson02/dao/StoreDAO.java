b78e09a1f5682836487b71a8da3127e1b241e649
JooYong <qlarslrs@naver.com>
2021-07-22 11:56:57 +0900

    [이주용] - myBatis 연동 연습 문제 1번 최초 파일 추가

---
+++ src/main/java/com/allured/test/lesson02/dao/StoreDAO.java
@@ -0,0 +1,12 @@
+package com.allured.test.lesson02.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.allured.test.lesson02.model.Store;
+
+@Repository
+public interface StoreDAO {
+	public List<Store> selectStoreList();
+}
